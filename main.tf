dc97ebe8d2cf8aaee28e94bceafbe4b39ceedbdb
Robert Peteuil <robert@peteuil.com>
2023-01-18 13:13:13 -0700

    adjust vars

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "vault" {
   # Address from environment variable VAULT_ADDR
   # auth_login_jwt {
   #   namespace = data.environment_variables.all.items["VAULT_NAMESPACE"]
-  #   mount     = var.vault_auth_login_jwt_mount
-  #   role      = var.vault_auth_login_jwt_role
-  #   jwt       = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
+  #   # mount     = var.vault_auth_login_jwt_mount
+  #   # role      = var.vault_auth_login_jwt_role
+  #   # jwt       = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
   # }
 }
 
